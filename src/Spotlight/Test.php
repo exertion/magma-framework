dcdea937777773b795906d48a447e7e3f567a0bf
Arif Ullah <aullah1@hotmail.co.uk>
2020-03-10 21:33:41 +0000

    Test class

---
+++ src/Spotlight/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spotlight;
+
+class Test
+{
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // return 'hi';
+        // parent::__construct();
+    }
+
+    public static function arif() {
+        return 'hi';
+    }
+}
